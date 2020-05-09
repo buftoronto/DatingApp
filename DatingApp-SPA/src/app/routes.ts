dd99e840e5796c04fa212b96c3725395304c524d
buftoronto <andy.weihuang@gmail.com>
2020-05-09 14:11:08 -0400

    End of Section 8.

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -6,7 +6,7 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
     {   path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
@@ -16,5 +16,5 @@ export const appRoutes: Routes = [
             { path: 'lists', component: ListsComponent },
         ]
     },
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
